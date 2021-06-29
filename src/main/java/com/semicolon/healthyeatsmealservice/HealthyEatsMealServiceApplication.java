077261244b57c4c7313e3c6eba7db87a8c757d0a
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-06-29 12:18:11 +0100

    project setup

---
+++ src/main/java/com/semicolon/healthyeatsmealservice/HealthyEatsMealServiceApplication.java
@@ -0,0 +1,13 @@
+package com.semicolon.healthyeatsmealservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyEatsMealServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthyEatsMealServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semicolon/healthyeatsmealservice/HealthyEatsMealServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.healthyeatsmealservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyEatsMealServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
